2e223e604ee7a57a512c54b63e0770113d14afd8
Vigneshlearneveryday <nauticalvigneshmenon@gmail.com>
2024-07-31 17:22:05 +0530

    Class work of React State

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Bulb from "./Bulb";
+import "./App.css";
+import Light from "./Light";
+function App() {
+  return (
+    <>
+      <Light></Light>
+      <Light></Light>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Bulb.jsx
@@ -0,0 +1,5 @@
+const Bulb = () => {
+  return <div className="bulb"> Bulb</div>;
+};
+
+export default Bulb;
